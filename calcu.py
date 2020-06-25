61aed944f8c7a0a0a7077fdf73e99ff783c2bf2b
AnderSon2777 <anderson.cordova@epn.edu.ec>
2020-06-25 08:53:53 -0500

    Creando el menu

---
+++ calcu.py
@@ -0,0 +1,12 @@
+def menu():
+	print("   CALCULADORA BASICA\n")
+	print(" Menu de opciones: \n")
+	print(" 1.- Suma")
+	print(" 2.- Resta")
+	print(" 3.- Multipliacion")
+	print(" 4.- Division")
+	op = int(input("Seleccione su opcion: "))
+	return (op)
+
+
+print(menu())
